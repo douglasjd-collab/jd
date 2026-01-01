1631389be925fed20c5e7f5ff941cbf5c901b557
JD PROMOTORA (Douglas Cavalcante) <douglas.jdpromotora@gmail.com>
2026-01-01 20:18:53 +0000

    File changes

--- src/components/forms/UsuarioForm.jsx
+++ src/components/forms/UsuarioForm.jsx
@@ -257,6 +257,7 @@ export default function UsuarioForm({ open, onOpenChange, usuario, onSubmit, isL
                 <SelectContent>
                   <SelectItem value="vendedor">Vendedor</SelectItem>
                   <SelectItem value="gerente">Gerente</SelectItem>
+                  <SelectItem value="admin">Administrador</SelectItem>
                   {currentUser?.perfil === 'master' && <SelectItem value="super_admin">Super Admin</SelectItem>}
                   {currentUser?.perfil === 'master' && <SelectItem value="master">Master</SelectItem>}
                 </SelectContent>
